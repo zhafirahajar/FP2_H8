422c70edbdac20e7a392f1b9c7e0909fc25411b9
Zhafirahajar <zfrhjr@gmail.com>
2021-12-05 18:54:12 +0700

    feat: add models validation

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,2 +1,118 @@
 const { User } = require("../models");
 const bcrypt = require("bcrypt");
+
+class userController {
+	static register(req, res) {
+		let salt = bcrypt.genSaltSync(10);
+		let hash = bcrypt.hashSync(req.body.password, salt);
+		let input = {
+			full_name: req.body.full_name,
+			email: req.body.email,
+			username: req.body.username,
+			password: hash,
+			profile_image_url: req.body.profile_image_url,
+			age: req.body.age,
+			phone_number: req.body.phone_number,
+			createdAt: new Date(),
+			updatedAt: new Date(),
+		};
+		User.create(input)
+			.then((data) => {
+				res.status(201).json(data);
+			})
+			.catch((err) => {
+				let errCode = 500;
+				if (err.name.includes("Sequelize")) {
+					errCode = 400;
+				}
+				res.status(errCode).json(err);
+			});
+	}
+
+	static async getAll(req, res) {
+		console.log(req.headers.token);
+		// User.findAll({
+		// 	include: [Major],
+		// })
+		// 	.then((data) => {
+		// 		res.json(data);
+		// 	})
+		// 	.catch((err) => {
+		// 		console.log(err);
+		// 	});
+
+		const userList = await User.findAll();
+		res.json(userList);
+	}
+
+	static async getOne(req, res) {
+		// User.findOne({
+		// 	where: {
+		// 		id: req.params.id,
+		// 	},
+		// })
+		// 	.then((data) => {
+		// 		if (data == null) {
+		// 			res.status(404).json(data);
+		// 		} else {
+		// 			res.status(200).json(data);
+		// 		}
+		// 	})
+		// 	.catch((err) => {
+		// 		res.status(500).json(err);
+		// 	});
+
+		const oneUser = await User.findOne({
+			where: {
+				id: req.params.id,
+			},
+		});
+		if (oneUser == null) {
+			res.status(404).json({ message: "User Not Found" });
+		} else {
+			res.status(200).json(oneUser);
+		}
+	}
+
+	static edit(req, res) {
+		User.update(
+			{
+				name: req.body.name,
+				age: req.body.age,
+				major_id: req.body.major_id,
+				password: req.body.password,
+			},
+			{
+				where: {
+					id: req.params.id,
+				},
+			}
+		)
+			.then((data) => {
+				res.status(201).json(data);
+			})
+			.catch((err) => {
+				res.status(500).json(err);
+			});
+	}
+
+	static delete(req, res) {
+		User.destroy({
+			where: {
+				id: req.params.id,
+			},
+		})
+			.then((data) => {
+				if (data > 0) {
+					res.status(200).json(data);
+				} else {
+					res.status(404).json(data);
+				}
+			})
+			.catch((err) => {
+				res.status(500).json(err);
+			});
+	}
+}
+
+module.exports = userController;
--- models/user.js
+++ models/user.js
@@ -1,29 +1,69 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  User.init({
-    full_name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    username: DataTypes.STRING,
-    password: DataTypes.STRING,
-    profile_image_url: DataTypes.TEXT,
-    age: DataTypes.INTEGER,
-    phone_number: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
-  return User;
+	class User extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+		}
+	}
+	User.init(
+		{
+			full_name: {
+				type: DataTypes.STRING,
+				validate: { notNull: true },
+			},
+			email: {
+				type: DataTypes.STRING,
+				validate: {
+					isEmail: true,
+					unique: true,
+					notNull: true,
+				},
+			},
+			username: {
+				type: DataTypes.STRING,
+				validate: {
+					notNull: true,
+					unique: true,
+				},
+			},
+			password: {
+				type: DataTypes.STRING,
+				validate: {
+					notNull: true,
+				},
+			},
+			profile_image_url: {
+				type: DataTypes.TEXT,
+				validate: {
+					isUrl: true,
+					notNull: true,
+				},
+			},
+			age: {
+				type: DataTypes.INTEGER,
+				validate: {
+					notNull: true,
+					isInt: true,
+				},
+			},
+			phone_number: {
+				type: DataTypes.INTEGER,
+				validate: {
+					notNull: true,
+					isInt: true,
+				},
+			},
+		},
+		{
+			sequelize,
+			modelName: "User",
+		}
+	);
+	return User;
 };
