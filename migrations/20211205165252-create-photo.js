0544103b9197b258edc66cd16b35f918e0a3678c
samansupriadi <samansenju@gmail.com>
2021-12-06 01:17:34 +0700

    make migration

---
+++ migrations/20211205165252-create-photo.js
@@ -0,0 +1,46 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Photos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      caption: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      poster_image_url: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      UserId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id'
+        },
+        onDelete: 'restrict',
+        onUpdate: 'restrict'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Photos');
+  }
+};
